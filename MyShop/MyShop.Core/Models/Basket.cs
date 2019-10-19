b32b313587eb0efea233fc6d984cf6cc6f7d304e
Justin <makebelief@icloud.com>
2019-10-19 16:39:04 +0800

    Added Basket &  Basket item (Basket Model) and migrated the database out to SQL

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity // basket item, then collection of basket items
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; } //lazy loading
+        
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
